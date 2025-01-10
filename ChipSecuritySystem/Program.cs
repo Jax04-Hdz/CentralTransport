7df6706e9bc625b84f37b010678cb9fea38e1ddb
Jax04-Hdz <jr.omaralex@gmail.com>
2025-01-10 13:07:49 -0600

    Add files via upload

---
+++ ChipSecuritySystem/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChipSecuritySystem
+{
+   internal class Program
+    {
+	private static readonly List<ColorChip> Chips = new List<ColorChip>();
+        // Random number generator
+        private static readonly Random Rand = new Random();
+
+
+
+        private static void Main(string[] args)
+        {
+	    // Clear console and display options
+            Console.Clear();
+            Console.WriteLine("Chips Required to Unlock Master Panel\n");
+            Console.WriteLine("1. Try your luck with randomly generated chips.");
+            Console.WriteLine("2. Manually enter chips.");
+            Console.WriteLine("3. Use example chips: [Blue, Yellow] [Red, Green] [Yellow, Red] [Orange, Purple].");
+            
+            int option;
+            while (!int.TryParse(Console.ReadLine(), out option) || (option != 1 && option != 2 && option != 3))
+            {
+                Console.WriteLine("Invalid input. Please enter 1, 2 or 3.");
+            }
+            
+            switch (option)
+            {
+                case 1:
+                    // Generate random chips
+                    GenerateChips();
+                    break;
+                case 2:
+                    // Get user input for chips
+                    InputChips();
+                    break;
+                case 3:
+                    // Generate Sample Chips
+                    Chips.Add(new ColorChip(Color.Blue, Color.Yellow));
+                    Chips.Add(new ColorChip(Color.Red, Color.Green));
+                    Chips.Add(new ColorChip(Color.Yellow, Color.Red));
+                    Chips.Add(new ColorChip(Color.Orange, Color.Purple));
+                    break;
+            }
+
+            Console.WriteLine("Your Chips");
+
+            foreach (var chip in Chips)
+            {
+                Console.WriteLine("[" + chip + "]");
+            }
+            Console.WriteLine(); // New line
+
+            var chipSet = new HashSet<ColorChip>();
+
+            foreach (var chip in Chips.Where(chip => chip.StartColor == Color.Blue))
+            {
+                chipSet.Add(chip);
+                if (FindChain(chip, chipSet))
+                {
+                    Console.WriteLine("Sequence\n----");
+
+                    foreach (var c in chipSet)
+                    {
+                        Console.WriteLine("[" + c + "]");
+                    }
+
+                    Console.WriteLine("\n Panel unlocked!\n");
+                    return;
+                }
+
+                chipSet.Remove(chip);
+            }
+	    Console.WriteLine(Constants.ErrorMessage + "!\n");
+        }
+
+        private static void GenerateChips()
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                var startColor = (Color)Rand.Next(0, 6);
+                var endColor = (Color)Rand.Next(0, 6);
+                Chips.Add(new ColorChip(startColor, endColor));
+            }
+        }
+
+        private static void InputChips()
+        {
+            Console.WriteLine("\nEnter colors for 4 chips. Choices:");
+
+            var values = Enum.GetValues(typeof(Color));
+            for (var j = 0; j < values.Length; j++)
+            {
+                Console.WriteLine($"{j + 1}. {values.GetValue(j)}");
+            }
+
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"\nChip {i + 1}:");
+
+                Color startColor = GetColorInput(nameof(startColor));
+                Color endColor = GetColorInput(nameof(endColor));
+
+                Chips.Add(new ColorChip(startColor, endColor));
+            }
+        }
+
+        private static Color GetColorInput(string colorType)
+        {
+            Console.Write($"{colorType}: ");
+
+            int input;
+            while (!int.TryParse(Console.ReadLine(), out input)
+                || input < 1 || input > Enum.GetValues(typeof(Color)).Length)
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+            return (Color)(input - 1);
+        }
+
+        private static bool FindChain(ColorChip lastChip, ISet<ColorChip> chipSet)
+        {
+            if (lastChip.EndColor == Color.Green)
+            {
+                return true;
+            }
+
+            foreach (var nextChip in Chips.Where(c => c.StartColor == lastChip.EndColor))
+            {
+
+                if (!chipSet.Add(nextChip)) continue;
+
+                if (FindChain(nextChip, chipSet))
+                {
+                    return true;
+                }
+
+                chipSet.Remove(nextChip);
+            }
+
+            return false;
+        }
+    }
+}
